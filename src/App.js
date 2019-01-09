3edc11a9a809336b89f0fa2c0094415d4996d78d
fancydevpro <fancydevpro@gmail.com>
2019-01-09 23:18:54 +0200

    add screens root

--- src/App.js
+++ src/App.js
@@ -1,9 +1,1 @@
-import { Navigation } from 'react-native-navigation'
 
-import Constants from '../global/Constants'
-
-import HomeScreen from './HomeScreen'
-
-export function registerScreens() {
-  Navigation.registerComponent(Constants.Screens.HOME_SCREEN.screen)
-}
