c043c69037b0b9328a113d1c7388c3878abf4f67
fancydevpro <fancydevpro@gmail.com>
2019-01-10 00:41:00 +0200

    add Home screen

--- src/Screens/index.js
+++ src/Screens/index.js
@@ -4,6 +4,6 @@ import Constants from '../global/Constants'
 
 import HomeScreen from './HomeScreen'
 
-export function registerScreens() {
+export default function registerScreens() {
   Navigation.registerComponent(Constants.Screens.HOME_SCREEN.screen, () => HomeScreen)
 }
