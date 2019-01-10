295676cbd2859883b6bfc700558d52072cd9da7d
fancydevpro <fancydevpro@gmail.com>
2019-01-10 17:47:23 +0200

    bind navigation events to home screen

--- src/Screens/HomeScreen/index.js
+++ src/Screens/HomeScreen/index.js
@@ -1,9 +1,55 @@
 import React, { Component } from 'react'
-import { View, Text } from 'react-native'
+import { 
+  View, 
+  Text,
+  Alert,
+} from 'react-native'
+import { Navigation } from 'react-native-navigation'
+
+import Constants from '../../global/Constants'
 
 export default class HomeScreen extends Component {
   constructor(props) {
     super(props)
+
+    Navigation.events().bindComponent(this)
+    this.isDrawerVisible = false
+  }
+
+  static get options() {
+    return {
+      topBar: {
+        borderColor: 'red',
+        leftButtons: [{
+          id: Constants.Buttons.HAMBURGER_BUTTON.id,
+          icon: Constants.Images.HAMBURGER,
+        }],
+        title: {
+          text: Constants.Screens.HOME_SCREEN.title,
+          alignment: 'center',
+        },
+        background: {
+          color: Constants.Colors.dodgerBlue,
+        }
+      }
+    }
+  }
+
+  navigationButtonPressed({buttonId}) {
+    if (buttonId === Constants.Buttons.HAMBURGER_BUTTON.id) {
+      this.toggleDrawer()
+    }
+  }
+
+  toggleDrawer() {
+    //this.isDrawerVisible = !this.isDrawerVisible
+    Navigation.mergeOptions(this.props.componentId, {
+      sideMenu: {
+        left: {
+          visible: true, //this.isDrawerVisible,
+        }
+      }
+    })
   }
 
   render() {
