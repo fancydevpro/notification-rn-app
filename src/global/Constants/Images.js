fd955adf3db1c06e43ec111b864c687215b2d9c0
fancydevpro <fancydevpro@gmail.com>
2019-01-10 18:49:11 +0200

    add plus images

--- src/global/Constants/Images.js
+++ src/global/Constants/Images.js
@@ -3,4 +3,5 @@ const PATH = '../../../assets/Images/'
 export default {
   HAMBURGER: require(`${PATH}hamburger.png`),
   ARROW_BACK: require(`${PATH}arrow-back.png`),
+  ROUND_PLUS: require(`${PATH}add-plus.png`),
 }
