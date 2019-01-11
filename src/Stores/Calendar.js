3bf5374100750e2e1036a60743d6855bcd4a4222
fancydevpro <fancydevpro@gmail.com>
2019-01-11 14:13:47 +0200

    add Calendar store

---
+++ src/Stores/Calendar.js
@@ -0,0 +1,10 @@
+import CalendarEvents from 'react-native-calendar-events'
+import moment from 'moment'
+
+export default class Calendar {
+  constructor(getStores) {
+    this.getStores = getStores
+  }
+
+  
+}
