80ccaf4e96f4d4bdeb60d67c563fde1cedb6d7b9
fancydevpro <fancydevpro@gmail.com>
2019-01-13 12:55:46 +0200

    add App store to global

--- src/Stores/App.js
+++ src/Stores/App.js
@@ -5,9 +5,12 @@ const APP_LAUNCH_FLAG_KEY = 'App.Launched'
 export default class {
   constructor(getStores) {
     this.getStores = getStores
+
+    this.getLaunchFlag = this.getLaunchFlag.bind(this)
+    this.setLaunchFlag = this.setLaunchFlag.bind(this)
   }
 
-  launchedApp = false
+  launchedAppFlag = false
 
   fetch = () => this.getLaunchFlag()
 
@@ -15,20 +18,19 @@ export default class {
     try {
       const value = JSON.parse(await AsyncStorage.getItem(APP_LAUNCH_FLAG_KEY))
       if (value !== null) {
-        this.launchedApp = value
+        this.launchedAppFlag = value
       } else {
-        this.launchedApp = false
+        this.launchedAppFlag = false
       }
     } catch (error) {
-      this.launchedApp = false
-      throw error
+      this.launchedAppFlag = false
     }
   }
 
   async setLaunchFlag() {
     try {
       await AsyncStorage.setItem(APP_LAUNCH_FLAG_KEY, JSON.stringify(true))
-      this.launchedApp = true
+      this.launchedAppFlag = true
     } catch (error) {
       throw error
     }
